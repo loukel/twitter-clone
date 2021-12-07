9f174007cc04396b04278d5dce7de1d1bfb4e166
loukel <louis.kelly1@hotmail.com>
2021-12-07 10:28:01 +0000

    Set up server file and node packages :sparkles:

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const morgan = require('morgan')
+const routes = require('./routes')
+
+// Create express app 🔨 
+const app = express()
+
+// Middleware
+app.use(morgan('dev'))
+app.use(express.json())
+
+// Routes 🐎
+app.use(express.static('public'))
+app.use(routes)
+
+// Start Server 🎉
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Web server listening on ${PORT}`)
+})
+
