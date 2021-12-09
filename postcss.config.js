e625ed66b6d16c3cce1f55e2b0ef830395ac8f11
loukel <louis.kelly1@hotmail.com>
2021-12-09 17:06:22 +0000

    Compiled tailwind css and added html

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// Reference on how to use tailwind css with express
+// https://dev.to/jumbo02/how-to-use-tailwind-css-in-an-expressjs-app-2c7c
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
