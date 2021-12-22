91be51d6039b3dafe48506b2525811ffe23295bd
Louis <65136145+loukel@users.noreply.github.com>
2021-12-22 23:06:01 +0000

    Added back button to PostView

--- public/js/features/post/PostView.js
+++ public/js/features/post/PostView.js
@@ -7,10 +7,17 @@ const PostView = async (id) => {
   return `
     <section class='body-font relative'>
       <button onclick='goHome()' class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute top-1 left-1'>
-        < Go Home
+        go back
       </button>
       <div class="container px-5 py-24 mx-auto">
         ${Post(post)}
+        <div class="mb-3 pt-0 flex">
+          <textarea id='postInput' oninput='handleInput()' onkeypress='handleKeyPress()' rows=2 maxlength=100 placeholder="what you thinking about this"
+            class="lowercase resize-none px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"></textarea>
+          <button onClick='submitPost()' class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            reply
+          </button>
+        </div>
       </div>
     </section>
   `
