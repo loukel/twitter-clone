33020abcade28f3a4200751db7eabc6859791155
Louis <65136145+loukel@users.noreply.github.com>
2021-12-24 23:37:17 +0000

    Improved post tests

--- routes/post.test.js
+++ routes/post.test.js
@@ -1,47 +1,44 @@
 import app from '../server'
-import supertest from 'supertest'
+import request from 'supertest'
 
 describe('Test post endpoints', () => {
   // Get_posts
   test('GET /api/posts succeeds', async () => {
-    const res = await supertest(app).get('/api/posts')
+    const res = await request(app).get('/api/posts')
     return expect(res.status).toBe(200)
   })
 
   // Create_post
-  let articleId = -1
+  let postId = -1
   test('POST /api/posts succeeds', async () => {
     const data = {
-      "title": "Cool article 3",
-      "authorUID": "1234543",
-      "body": "Lorem300",
+      "body": "lorem300",
     }
-    const res = await supertest(app).post('/api/posts')
+    const res = await request(app).post('/api/posts')
       .send(data)
-    articleId = res.body.id
+    postId = res.body.id
     return expect(res.status).toBe(201)
   })
 
-  // Get_article
+  // Get_post
   test('GET /api/posts/:id succeeds', async () => {
-    const res = await supertest(app).get(`/api/posts/${articleId}`)
+    const res = await request(app).get(`/api/posts/${postId}`)
     return expect(res.status).toBe(200)
   })
 
-  // Update_article
+  // Update_post
   test('PUT /api/posts/:id succeeds', async () => {
     const data = {
-      "title": "Not Cool article 3",
       "body": "Lorem301",
     }
-    const res = await supertest(app).put(`/api/posts/${articleId}`)
+    const res = await request(app).put(`/api/posts/${postId}`)
       .send(data)
     return expect(res.status).toBe(204)
   })
 
-  // Delete_article
+  // Delete_post
   test('DELETE /api/posts/:id succeeds', async () => {
-    const res = await supertest(app).delete(`/api/posts/${articleId}`)
+    const res = await request(app).delete(`/api/posts/${postId}`)
     return expect(res.status).toBe(204)
   })
 })
