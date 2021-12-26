153ab0046350a80e7062de17d9451413cf3f2f0b
Louis <65136145+loukel@users.noreply.github.com>
2021-12-26 16:19:22 +0000

    Added error handling to login

--- public/js/features/auth/LoginModal.js
+++ public/js/features/auth/LoginModal.js
@@ -4,6 +4,11 @@ import {
 }
 from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js"
 
+/*
+ * Modal modifed from https://tailwindui.com/components/application-ui/overlays/modals
+ * Alert modified from https://v1.tailwindcss.com/components/alerts
+ */
+
 const LoginModal = () => {
   const auth = getAuth()
 
@@ -24,6 +29,10 @@ const LoginModal = () => {
       .then(() => {
         window.rerender()
       })
+      .catch(() => {
+        document.getElementById('loginErrorMessage').innerHTML = "Incorrect email/password combination entered or account doesn't exist"
+        document.getElementById('loginErrorAlert').classList.remove('hidden')
+      })
   }
 
   return `
@@ -37,6 +46,9 @@ const LoginModal = () => {
         <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
           <form class="bg-white px-4 py-8 pb-4 rounded sm:p-6 sm:pb-4" onSubmit='submitLogin()'>
             <h1 class="mb-8 text-3xl text-center">Login</h1>
+            <div id='loginErrorAlert' class="mb-3 hidden bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+              <span class="block sm:inline" id='loginErrorMessage'></span>
+            </div>
             <input 
                 type="text"
                 id='loginEmail'
