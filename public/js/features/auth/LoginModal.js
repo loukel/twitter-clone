78d51bac90de8e6607f15d1dbf18acb6fefef897
loukel <louis.kelly1@hotmail.com>
2022-01-25 19:31:47 +0000

    Login and register more responsive

--- public/js/features/auth/LoginModal.js
+++ public/js/features/auth/LoginModal.js
@@ -58,7 +58,7 @@ const LoginModal = () => {
 
   return `
     <div id='loginModal' class="hidden fixed z-10 inset-0 overflow-y-auto">
-      <div id='backgroundElement' class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div id='backgroundElement' class="flex items-end justify-center min-h-screen pt-4 px-4 pb-96 md:pb-20 text-center sm:block sm:p-0">
         <div onClick='hideLoginModal()' class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
         <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
